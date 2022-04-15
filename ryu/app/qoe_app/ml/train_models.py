b70d10b7b74ec9f48f9876831f5299b02aca30ca
adam-durning <adam.durning@ucdconnect.ie>
2022-04-15 16:35:53 +0100

    Repo Cleanup: Deleting files and directories that are no longer needed

--- ryu/app/qoe_app/ml/train_models.py
+++
@@ -1,5 +0,0 @@
-import ml_models_applying_new as model
-
-model.MlModles(3)
-
-
--- ryu/app/qoe_app_lei/setting.py
+++
@@ -1,9 +0,0 @@
-DISCOVERY_PERIOD = 3   			# For discovering topology.
-
-MONITOR_PERIOD = 3				# For monitoring traffic
-
-DELAY_DETECTING_PERIOD = 3			# For detecting link delay.
-
-TOSHOW = True						# For showing information in terminal
-	
-MAX_CAPACITY = 281474976710655		# Max capacity of link
